4b80bb6a087bf016221c8d2226b4d26895624317
Paulo Ricardo <paulo.ricardo1137.pr@gmail.com>
2022-02-21 23:53:51 +0000

    creat list display to Point on admin

--- core/admin.py
+++ core/admin.py
@@ -1,6 +1,15 @@
 from django.contrib import admin
 from .models import Employee, Point
 
+
+class PointAdmin(admin.ModelAdmin):
+    list_display = (
+        'employee',
+        'day_of_week',
+        'date',
+    )
+
+
 admin.site.register(Employee)
-admin.site.register(Point)
+admin.site.register(Point, PointAdmin)
 
