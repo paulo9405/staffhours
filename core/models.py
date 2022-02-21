c8a281e5c6abe52b263c0b4768b94082f37444a5
Paulo Ricardo <paulo.ricardo1137.pr@gmail.com>
2022-02-21 20:39:06 -0300

    first commit

---
+++ core/models.py
@@ -0,0 +1,20 @@
+from django.db import models
+
+
+class Employee(models.Model):
+    name = models.CharField(max_length=100)
+    adress = models.CharField(max_length=200)
+    phone = models.CharField(max_length=20)
+    position = models.CharField(max_length=100)
+
+
+    def __str__(self):
+        return self.name
+
+
+class Point(models.Model):
+    employee = models.ForeignKey(Employee, on_delete=models.CASCADE)
+    date = models.DateField()
+    day_of_week = models.CharField(max_length=30)
+    #entrance
+    #left
